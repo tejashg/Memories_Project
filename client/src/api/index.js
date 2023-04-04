336178f7448ccca51ad5e457b3b83012fe6b2e3f
Tejash Gupta <tejashg790@gmail.com>
2023-04-04 19:15:51 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: process.env.REACT_BACKEND_URL });
+const API = axios.create({ baseURL: process.env.REACT_APP_BACKEND_URL });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
